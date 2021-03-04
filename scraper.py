3369c8ffd2d76f92c9b52857b2dcb293e40a2439
Aaron Rine <aaronrine@gmail.com>
2021-03-04 14:57:58 -0800

    refactor(scraper): condense if statement and shorten names

--- scraper.py
+++ scraper.py
@@ -5,12 +5,8 @@ html = requests.get('https://www.marketwatch.com/tools/marketsummary').text
 soup = BeautifulSoup(html, 'lxml')
 table = soup.find("table", id="marketsummaryindexes")
 
-head = table.find("th")
-
-table_data = table.find_all("td")
-for raw_table_data in table_data:
-    if(raw_table_data.a):
-        processed_table_data = raw_table_data.a.text
-    else:
-        processed_table_data = raw_table_data.text
-    print(processed_table_data)
+all_table_data = table.find_all("td")
+for table_data in all_table_data:
+    if (table_data.a):
+        table_data = table_data.a
+    print(table_data.text)
